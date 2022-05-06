f1ede0d8e400d05ff949c5233b97ed2ba3a32fa7
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-05-06 15:08:00 -0400

    Update - 5/6/22

--- SpeedTester.py
+++ SpeedTester.py
@@ -1,7 +1,12 @@
 # Built-in Modules #
+import errno
+import json
+import logging
 import os
 import shlex
+import sys
 import time
+from datetime import date, datetime
 
 # Third-party modules #
 from speedtest import Speedtest
@@ -52,6 +57,7 @@ def IntervalSleepCounter(result_dict: dict, time_interval: int, clear_display):
         time.sleep(1)
         counter -= 1
 
+    os.system(clear_display)
 
 '''
 ########################################################################################################################
@@ -78,7 +84,57 @@ def RunTest(servers: list, threads: None) -> dict:
     # Parse test results as dict #
     results = test.results.dict()
 
+    curr_time = datetime.now()
+    report_name = f'SpeedtestReport_{date.today()}_{curr_time.hour}-{curr_time.minute}.txt'
+
+    try:
+        # Open report file in append mode #
+        with open(report_name, 'a') as in_file:
+            # Write the name of the current file to report file #
+            in_file.write(f'{report_name}\n{(1 + len(report_name)) * "*"}\n')
+            # Write json results to output report file #
+            json.dump(results, in_file, sort_keys=False, indent=4)
+            in_file.write('\n\n')
+
+    # If IO error occurs #
+    except IOError as err:
+        # IF file does not exist #
+        if err.errno == errno.ENOENT:
+            PrintErr(f'{report_name} does not exist')
+            logging.exception(f'{report_name} does not exist\n\n')
+            sys.exit(-1)
+
+        # If the file does not have read/write access #
+        elif err.errno == errno.EPERM:
+            PrintErr(f'{report_name} does not have permissions for append file mode, if file exists confirm it is closed')
+            logging.exception(f'{report_name} does not have permissions for append file mode\n\n')
+            sys.exit(-2)
+
+        # File IO error occurred #
+        elif err.errno == errno.EIO:
+            PrintErr(f'IO error occurred during append mode on {report_name}')
+            logging.exception(f'IO error occurred during append mode on {report_name}\n\n')
+            sys.exit(-3)
+
+        # If other unexpected file operation occurs #
+        else:
+            PrintErr(f'Unexpected file operation occurred accessing {report_name}: {err.errno}')
+            logging.exception(f'Unexpected file operation occurred accessing {report_name}: {err.errno}\n\n')
+            sys.exit(-4)
+
     return results
+
+
+"""
+########################################################################################################################
+Name:       PrintErr
+Purpose:    Displays error message through standard output.
+Parameters: The error message to be displayed.
+Returns:    None
+########################################################################################################################
+"""
+def PrintErr(msg: str):
+    print(f'\n* [ERROR] {msg} *\n', file=sys.stderr)
 
 
 '''
@@ -102,13 +158,13 @@ def UserInput() -> tuple:
             # If the number is not even or intervals are less than 2 or greater than 12 #
             if intervals % 2 != 0 or intervals < 2 or intervals > 12:
                 # Print error, provide suggestion and re-iterate loop #
-                print('\n* Improper input .. enter a number in between 2 and 12 *\n')
+                PrintErr('Improper input .. enter a number in between 2 and 12')
                 continue
 
             # If hours is less than 0 or hours is greater than max constant #
             if hours < 0 or hours > MAX_HOURS:
                 # Print error, provide suggestion and re-iterate loop #
-                print(f'\n* Improper input .. avoid numbers below 0 or above {MAX_HOURS} *\n')
+                PrintErr(f'Improper input .. avoid numbers below 0 or above {MAX_HOURS}')
                 continue
 
             # If the user wants a single test #
@@ -117,7 +173,7 @@ def UserInput() -> tuple:
 
         except ValueError:
             # Print error, provide suggestion and re-iterate loop #
-            print('\n* Improper input .. enter a number not other data type *\n')
+            PrintErr('Improper input .. enter a number not other data type')
             continue
 
         return intervals, hours
@@ -132,8 +188,11 @@ Returns:    Nothing
 ########################################################################################################################
 '''
 def main():
+    # Set the log file name #
+    logging.basicConfig(level=logging.DEBUG, filename='.\\BandwidthTesterLog.log')
+
     servers = []
-    threads= None
+    threads = None
     cmds = ('cls', 'clear')
 
     # If the OS is Windows #
