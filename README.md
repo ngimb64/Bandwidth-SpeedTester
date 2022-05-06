f937d12e23b7a270c3d11f0660848e33568a167e
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-05-06 14:34:51 -0400

    Update - 5/6/22

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# BandwidthSpeed-Tester
+# Bandwidth-SpeedTester
+![alt text](https://github.com/ngimb64/Bandwidth-Speedtester/blob/main/BandwidthSpeedTester.png?raw=true)
 
 ## Prereqs
-Written on Windows 10 with Python 3.9
+> This program runs on Windows and Linux, written in Python 3.9
 
 ## Installation
-Run the setup.py script to install third-party packages
+- Run the setup.py script to build a virtual environment and install all external packages in the created venv.
+
+> Example:<br>
+> python3 setup.py "venv name"
+
+- Once virtual env is built traverse to the Scripts directory in the environment folder just created.
+- In the Scripts directory, execute the "activate" script to activate the virtual environment.
+
 
 ## Purpose
-Test connenction bandwidth as a singular test or an interval-based time series.
+> Test connection bandwidth as a singular test or an interval-based time series through the Speed Test API.
+
+## How to use
+- Open up Command Prompt (CMD) or terminal
+- Enter the directory containing the program and execute in shell
--- speed_test.py
+++ speed_test.py
@@ -1,27 +1,76 @@
 # Built-in Modules #
-from time import sleep
+import os
+import shlex
+import time
 
 # Third-party modules #
 from speedtest import Speedtest
 
+
 # Pseudo-Constant #
 MAX_HOURS = 24
+
+
+'''
+########################################################################################################################
+Name:       PrintResultDict
+Purpose:    Displays the speed test results from passed in dictionary.
+Parameters: Dictionary of speed test results to be displayed.
+Returns     Nothing
+########################################################################################################################
+'''
+def PrintResultDict(result: dict):
+    # Print the results #
+    print(f'Ping: {result["ping"]}')
+    print(f'Download: {result["download"] / 8_000_000}')
+    print(f'Upload: {result["upload"] / 8_000_000}\n')
+
+    # Iterate through server dict and print data #
+    [print(f'{key}: {value}') for key, value in result["server"].items()]
+    print('')
+
+
+'''
+########################################################################################################################
+Name:       IntervalSleepCounter
+Purpose:    Displays sleep counter based on user specified intervals.
+Parameters: Time interval to sleep.
+Returns     Nothing
+########################################################################################################################
+'''
+def IntervalSleepCounter(result_dict: dict, time_interval: int, clear_display):
+    counter = time_interval
+
+    # Iterate through time interval range printing and sleeping each second #
+    for second in range(1, int(time_interval) + 2):
+        # Clear the screen #
+        os.system(clear_display)
+        # Print the speed test results #
+        PrintResultDict(result_dict)
+        print(f'Time until next test: {counter}')
+        print(f'{second * "!"}')
+        time.sleep(1)
+        counter -= 1
+
 
 '''
 ########################################################################################################################
 Name:       RunTest
 Purpose:    Executes test and prints results.
 Parameters: The list of connection testing servers and job threads.
-Returns:    None
+Returns:    A dictionary containing speed test results.
 ########################################################################################################################
 '''
-def RunTest(servers, threads):
+def RunTest(servers: list, threads: None) -> dict:
     # Initialize test object #
     test = Speedtest()
     # Get list of available servers #
     test.get_servers(servers)
     # Get the best available server #
-    test.get_best_server()
+    best = test.get_best_server()
+
+    print(f'\nRunning test on server => {best}\n')
+
     # Test download #
     test.download(threads=threads)
     # Test upload #
@@ -29,24 +78,18 @@ def RunTest(servers, threads):
     # Parse test results as dict #
     results = test.results.dict()
 
-    # Print the results #
-    print(f'Download: {results["download"] / 8000000}')
-    print(f'Upload: {results["upload"] / 8000000}')
-    print(f'Ping: {results["ping"]}')
-
-    # Iterate through server dict and print data #
-    [print(f'{key}: {value}') for key, value in results["server"].items()]
+    return results
 
 
 '''
 ########################################################################################################################
 Name:       UserInput
 Purpose:    Prompts user for the number of test intervals in an hour and how many hours to test.
-Parameters: None
-Returns:    Validated user input.
+Parameters: Nothing
+Returns:    Validated user input of number of intervals and hours.
 ########################################################################################################################
 '''
-def UserInput():
+def UserInput() -> tuple:
     # Iterate until results are returned #
     while True:
         try:
@@ -84,13 +127,21 @@ def UserInput():
 ########################################################################################################################
 Name:       main
 Purpose:    Prompts user, calculates intervals, executes bandwidth tests in a singular or time series fashion.
-Parameters: None
-Returns:    None
+Parameters: Nothing
+Returns:    Nothing
 ########################################################################################################################
 '''
 def main():
     servers = []
-    threads = None
+    threads= None
+    cmds = ('cls', 'clear')
+
+    # If the OS is Windows #
+    if os.name == 'nt':
+        clear_cmd = shlex.quote(cmds[0])
+    # If the OS is Linux #
+    else:
+        clear_cmd = shlex.quote(cmds[1])
 
     # Get the users input #
     intervals, hours = UserInput()
@@ -108,7 +159,7 @@ def main():
             # Decremental counter loop #
             while count != 0:
                 # Check internet speed through speedtest api #
-                RunTest(servers, threads)
+                res = RunTest(servers, threads)
                 # Reset server list #
                 servers = []
 
@@ -118,7 +169,7 @@ def main():
 
                 # Sleep program interval time
                 # until the next test #
-                sleep(interval_time)
+                IntervalSleepCounter(res, interval_time, clear_cmd)
 
                 count -= 1
 
@@ -126,7 +177,9 @@ def main():
             count = intervals
     else:
         # Check internet speed through speedtest api #
-        RunTest(servers, threads)
+        res = RunTest(servers, threads)
+        # Display speed test results #
+        PrintResultDict(res)
 
 
 if __name__ == '__main__':
